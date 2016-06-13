d2de5ce9bfa1b1036c418729db4f1c9da7aa1156
Ismael Ors <ismikin@gmail.com>
2016-06-13 17:50:31 +0200

    Previous remove login route

---
+++ app/scripts/directives/navigation.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name ngSimpleGroupsApp.directive:navigation
+ * @description
+ * # navigation
+ */
+angular.module('ngSimpleGroupsApp')
+  .directive('navigation', function () {
+    return {
+      templateUrl: 'views/navigation.html',
+      restrict: 'E',
+      transclude: true,
+      link: angular.noop,
+      controller: NavigationController,
+      controllerAs: 'navigation',
+      bindToController: true,
+    };
+
+    function NavigationController(authService) {
+      var navigation = this;
+
+      navigation.loggedIn = authService.loggedIn;
+    }
+  });
